ed691f8de9c163a8004e4880bbe214f347175f60
WARRIORORDEFENDER <abdugani.lapasov2005@gmail.com>
2023-12-15 01:29:30 +0500

    models and tables created

---
+++ models/models.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+from sqlalchemy import Table, Column, String, Integer, Text, Boolean, MetaData, TIMESTAMP, Date, ForeignKey
+
+metadata = MetaData()
+
+user = Table(
+    'user',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('first_name', String, nullable=True),
+    Column('last_name', String, nullable=True),
+    Column('email', String),
+    Column('phone', String),
+    Column('username', String),
+    Column('password', String),
+    Column('birth_date', Date),
+    Column('registered_date', TIMESTAMP, default=datetime.utcnow),
+    Column('last_login', TIMESTAMP)
+)
+
+role = Table(
+    'role',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String),
+    Column('user_id', Integer, ForeignKey('user.id'))
+)
+
+product = Table(
+    'product',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String),
+    Column('owner_id', Integer, ForeignKey('user.id')),
+    Column('price', Integer, default=0),
+    Column('sold_count', Integer, default=0)
+)
+
+user_saved_items = Table(
+    'user_saved_items',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('product_id', Integer, ForeignKey('product.id')),
+    Column('created_at', TIMESTAMP)
+)
+
+comment = Table(
+    'comment',
+    metadata,
+    Column('id',Integer,primary_key=True,autoincrement=True),
+    Column('user_id',Integer,ForeignKey('user.id')),
+    Column('product_id',Integer,ForeignKey('product.id')),
+    Column('message',Text)
+)
+
+
+
+hashtags = Table(
+    'hashtags',
+    metadata,
+    Column('id',Integer,primary_key=True,autoincrement=True),
+    Column('name',String),
+    Column('product_id',ForeignKey('product.id'))
+)
+
+
+category = Table(
+    'category',
+    metadata,
+    Column('id',Integer,primary_key=True,autoincrement=True),
+    Column('name',String),
+    Column('product_id',ForeignKey('product.id'))
+)
+
+
+topic = Table(
+
+    'topic',
+    metadata,
+    Column('id',Integer,primary_key=True,autoincrement=True),
+    Column('name',String),
+    Column('product_id',ForeignKey('product.id'))
+)
