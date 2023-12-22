a89f6f6d62d6d7567f582031a7fce218a7fe970c
itm10 <ibrohim.islom10@hmail.com>
2023-12-22 20:03:19 +0500

    changes

--- auth/auth.py
+++ auth/auth.py
@@ -9,13 +9,16 @@ from fastapi import Depends, APIRouter, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from passlib.context import CryptContext
 
+from sqlalchemy import select, insert, update, delete
+
 from auth.utils import generate_token, verify_token
 from database import get_async_session
-from models.models import user
+from models.models import user, user_saved_items
 
-from auth.schemes import UserInfo, UserCreate, User_In_db, UserLogin
+from auth.schemes import UserInfo, UserCreate, User_In_db, UserLogin, UserInDB, UserSaveScheme
 
 user_register_router = APIRouter()
+user_information = APIRouter()
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
@@ -68,3 +71,40 @@ async def user_info(token: dict = Depends(verify_token), session: AsyncSession =
         return UserInfo(**user_data._asdict())
     except NoResultFound:
         raise HTTPException(status_code=404, detail='User not found !!!')
+
+
+@user_information.post('/user-save-items')
+async def user_saved(data_id: UserSaveScheme, session: AsyncSession = Depends(get_async_session)):
+    query1 = select(user_saved_items).where(user_saved_items.c.user_id == data_id.user_id).where(
+        user_saved_items.c.product_id == data_id.product_id)
+    user_exist = await session.execute(query1)
+    exist = bool(user_exist.scalar())
+    if exist:
+        return {'success': 'User has already liked'}
+
+    query = UserInDB(**dict(data_id), created_at=datetime.utcnow())
+    user_data = insert(user_saved_items).values(**dict(query))
+    await session.execute(user_data)
+    await session.commit()
+    return {'success': True}
+
+
+@user_information.delete('/user-save-items')
+async def delete_favorites(data_id: UserSaveScheme, session: AsyncSession = Depends(get_async_session)):
+    delete_query = select(user_saved_items).where(
+        user_saved_items.c.user_id == data_id.user_id,
+        user_saved_items.c.product_id == data_id.product_id
+    )
+    exist_data = await session.execute(delete_query)
+    exist = bool(exist_data.scalar())
+    if exist:
+        query = delete(user_saved_items).where(
+            user_saved_items.c.user_id == data_id.user_id,
+            user_saved_items.c.product_id == data_id.product_id
+        )
+        await session.execute(query)
+        await session.commit()
+        return {'success': True, 'message': 'Successfully deleted'}
+    else:
+        return {'success': False, 'message': 'User have not liked it'}
+
